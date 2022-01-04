445f60970e71b4757cf208b8e5423425cb913d87
Hephaestus <katie@Hephaestus.Hephaestus.com>
2022-01-04 01:01:19 -0800

    Adding in basic identification: name, level, race, job

---
+++ basicInfo.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+var name, race, job string
+var level int
+
+func basicInfo() {
+	input := bufio.NewScanner(os.Stdin)
+	fmt.Println("What is your character's name?")
+	input.Scan()
+	name = input.Text()
+
+	fmt.Printf("What level is %v?\n", name)
+	fmt.Scan(&level)
+
+	input = bufio.NewScanner(os.Stdin)
+	fmt.Printf("What race is %v?\n", name)
+	input.Scan()
+	race = input.Text()
+
+	input = bufio.NewScanner(os.Stdin)
+	fmt.Println("Which class will you be playing?")
+	input.Scan()
+	job = input.Text()
+
+	fmt.Printf("%v is a level %v %v %v.\n", name, level, race, job)
+}
