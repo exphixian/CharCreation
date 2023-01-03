c6eab038b4260682a4868ec33eb47b1937bf436f
exphixian <37256276+exphixian@users.noreply.github.com>
2023-01-03 02:54:49 -0800

    Update diceroll.go

--- diceroll.go
+++ diceroll.go
@@ -9,7 +9,7 @@ func roll(sides int, dice int) []int {
 	rand.Seed(time.Now().UnixNano())
 	results := []int{}
 	for i := 0; i < dice; i++ {
-		results = append(results, rand.Intn(sides))
+		results = append(results, rand.Intn(sides) + 1)
 	}
 	return results
 }
